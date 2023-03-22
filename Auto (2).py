21f0884f8f03785b21f551fe4626fb42c8ba209e
jpantojavelasco <128170706+jpantojavelasco@users.noreply.github.com>
2023-03-22 20:12:49 +0000

    Negritas, porcentajes en rojo

--- Auto (2).py
+++ Auto (2).py
@@ -3,13 +3,14 @@ import openpyxl
 from openpyxl import Workbook 
 from openpyxl.styles import Font, Alignment 
 from openpyxl.styles import PatternFill
+from openpyxl.styles import Border, Side
 from openpyxl import load_workbook
 from openpyxl.worksheet.page import PageMargins
 from openpyxl.utils import get_column_letter
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
-
+import re
 
 
 # Leer el archivo CSV
@@ -123,6 +124,49 @@ for idx, row in enumerate(ws.iter_rows(),1):
         change = True
     for cell in row:
         cell.fill = fill
+        
+# Poner el renglon de total en negritas y porcentajes negativos en rojo
+for idx, row in enumerate(ws.iter_rows(),1):
+    if str(ws.cell(row=idx, column=4).value) == 'TOTAL':
+        for cell in row:
+            cell.font = Font(bold=True, name='Calibri', size=6)
+        if re.search("-", str(ws.cell(row=idx, column=15).value)):
+            ws.cell(row=idx, column=15).font = Font(color = "FF0000", name='Calibri', size=6, bold=True)
+    elif re.search("-", str(ws.cell(row=idx, column=15).value)):
+         ws.cell(row=idx, column=15).font = Font(color = "FF0000", name='Calibri', size=6)
+
+columnaP = ws['C']
+columnaU = ws['O']
+mexterior = Side(style = 'thin', color = 'BFBFBF')
+
+# Pintar los bordes verticales exteriores
+for celda in columnaP:
+    celda.border = Border(left = mexterior)
+for celda in columnaU:
+    celda.border = Border(right = mexterior)
+
+columnas = ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
+mnormal = Side(style = 'thin', color = 'F2F2F2')
+
+# Pintar los bordes verticales interiores
+for idx, value in enumerate(columnas,0):
+    for idxcelda, celda in enumerate(ws[columnas[idx]],0):
+        if idxcelda != 0:
+            celda.border = Border(right = mnormal)
+
+    
+#Pintar los bordes totales
+
+for idx, row in enumerate(ws.iter_rows(),1):
+    if str(ws.cell(row=idx, column=4).value) == 'TOTAL':
+        for idxcell, celda in enumerate(row,1):
+            if re.search("Cell 'Sheet'.C", str(celda)):
+                celda.border = Border(top = mnormal, bottom = mnormal, left = mexterior, right = mnormal)
+            elif re.search("Cell 'Sheet'.O", str(celda)):
+                celda.border = Border(top = mnormal, bottom = mnormal, left = mnormal, right = mexterior)
+            else:
+                celda.border = Border(top = mnormal, bottom = mnormal, left = mnormal, right = mnormal)
+
 
 # Establecer color de relleno para los encabezados
 fill = PatternFill(start_color='BFBFBF', end_color='BFBFBF', fill_type='solid')
@@ -156,9 +200,8 @@ for celda in columna:
     if isinstance(celda.value, float):  # verifica si el valor de la celda es un decimal
         celda.value = celda.value * 1  # convierte el valor a porcentaje
         celda.number_format = '0%'  # establece el formato de número de la celda como porcentaje con dos decimales
-
-
-
+    if (re.search("-", str(celda.value))):
+        celda.value = str(int(celda.value * 100)).replace('-','(') + ')%'
 
 # Indicar el número de columna que deseas eliminar
 num_columna = 1
