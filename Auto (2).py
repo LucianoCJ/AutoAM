7255bdcf27900ed28943f58ad2b18669a3388305
jpantojavelasco <jp.velasco@etribe.mx>
2023-04-28 13:28:38 -0600

    Cabeceras en html

--- Auto (2).py
+++ Auto (2).py
@@ -28,15 +28,13 @@ before_sunday = today - datetime.timedelta(days=today.weekday() + 1)
 # Dar formato a la fecha como mes-día-año
 formatted_date = today.strftime("%b %d, %Y").replace("{0:0>2}".format(today.day), str(today.day) + ("th" if 11<= today.day <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(today.day % 10, 'th')),1)
 formatted_date2 = next_sunday.strftime("%b %d").replace("{0:0>2}".format(next_sunday.day), str(next_sunday.day) + ("th" if 11<= next_sunday.day <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(next_sunday.day % 10, 'th')),1)
-
 formatted_date3 = before_sunday.strftime("%b %d").replace("{0:0>2}".format(before_sunday.day), str(before_sunday.day) + ("th" if 11<= before_sunday.day <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(before_sunday.day % 10, 'th')),1)
-
 
 #Fecha del archivo
 formatted_date4 = next_sunday.strftime("%b%d%Y")
 
 ############################################################
-originFile = 'ReporteOAGUS_20230312.csv'
+originFile = 'ReporteOAGLATAM_20230312.csv'
 # Leer el archivo CSV
 df = pd.read_csv(originFile, header = None)
 
@@ -96,7 +94,6 @@ for idx, cell in enumerate(ws[1],1):
     else:
         cell.alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
 
-
 # Escribir los datos                
 for row_num, row_data in enumerate(df.values, 2):
     for col_num, cell_value in enumerate(row_data, 2):
@@ -122,7 +119,6 @@ color1 = 'F2F2F2'
 color2 = 'FFFFFF'
 change = False
 
-
 # cambiar el formato de color de los renglones 
 for idx, row in enumerate(ws.iter_rows(),1):
     if ((str(ws.cell(row=idx, column=3).value) == 'None') or (str(ws.cell(row=idx, column=4).value) == 'TOTAL')):
@@ -146,41 +142,6 @@ for idx, row in enumerate(ws.iter_rows(),1):
             ws.cell(row=idx, column=15).font = Font(color = "FF0000", name='Calibri', size=6, bold=True)
     elif re.search("-", str(ws.cell(row=idx, column=15).value)):
          ws.cell(row=idx, column=15).font = Font(color = "FF0000", name='Calibri', size=6)
-
-columnaP = ws['C']
-columnaU = ws['O']
-mexterior = Side(style = 'thin', color = 'BFBFBF')
-
-columnas = ['C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
-mnormal = Side(style = 'thin', color = 'F2F2F2')
-
-# Pintar los bordes verticales interiores
-#for idx, value in enumerate(columnas,0):
-#    for idxcelda, celda in enumerate(ws[columnas[idx]],0):
-#        if idxcelda != 0:
-#            celda.border = Border(right = mnormal)
-
-# Pintar los bordes verticales exteriores
-#for idx, celda in enumerate(columnaP,1):
-#    if idx == 1:
-#        celda.border = Border(left = mexterior)
-#    else:
-#        celda.border = Border(left = mexterior, right = mnormal)
-#for celda in columnaU:
-#    celda.border = Border(right = mexterior)
-
-#Pintar los bordes totales
-
-#for idx, row in enumerate(ws.iter_rows(),1):
-#    if str(ws.cell(row=idx, column=4).value) == 'TOTAL':
-#        for idxcell, celda in enumerate(row,1):
-#            if re.search("Cell 'Sheet'.C", str(celda)):
-#                celda.border = Border(top = mnormal, bottom = mnormal, left = mexterior, right = mnormal)
-#            elif re.search("Cell 'Sheet'.O", str(celda)):
-#                celda.border = Border(top = mnormal, bottom = mnormal, left = mnormal, right = mexterior)
-#            else:
-#                celda.border = Border(top = mnormal, bottom = mnormal, left = mnormal, right = mnormal)
-
 
 # Establecer color de relleno para los encabezados
 fill = PatternFill(start_color='BFBFBF', end_color='BFBFBF', fill_type='solid')
@@ -210,40 +171,18 @@ num_cols = ws.max_column
 # Establecer el estilo de fuente y alineación para las cabeceras
 font = Font(name='Calibri', size=6, bold=True)
 alignment = Alignment(horizontal='center', vertical='center', wrap_text=True)
-# Define el número de filas entre cada repetición de la cabecera
-#n = 48
 
-# Itera a través de las filas y agrega las cabeceras cada n filas
-#for i in range(1, num_rows + 1):
-    #if (i - 1) % n == 0:
-         #Copia las celdas de la primera fila a la fila actual
-        #for j in range(1, num_cols + 1 ):
-for max_row in range(2, num_rows + 35): #el 35 es un numero arbitrario para que aparezca la cabecera en la ultima pagina de todos los archivos
-    if (max_row - 1) % 79 == 0:
-        # Inserta una nueva fila en la parte superior de la página
-        ws.insert_rows(max_row)
-        ws.row_dimensions[max_row].height = 17.25
-        for col_num in range(1, num_cols + 1):
-            cell = ws.cell(row=max_row, column=col_num)
-            cell_name =get_column_letter(col_num) + '1'
-            #header_cell = ws.cell(row=1, column=j)
-            header_cell = ws[cell_name]
-            cell.value = header_cell.value
-            cell.font = font
-            if col_num <= 8:
-                cell.alignment = Alignment(horizontal='left', vertical='center', wrap_text=True)
-            else:
-                cell.alignment = alignment
-            cell.fill = fill    #Agrego el color de relleno en el renglon 1
-
+#Cambiar la region en el nombre del archivo dependiendo del archivo de origen
+region=''
+if re.match("^ReporteOAGLATAM",originFile):
+    region = " LATAM"
+elif re.match("^ReporteOAGUS",originFile):
+    region = " USA"
 
 ################################################################################
 
 # Crear un objeto HeaderFooter y asignarle los textos
-#hf = HeaderFooter()
-#hf.center_header.text = "Encabezado"
-#hf.center_footer.text = "Pie de página"
-ws.oddHeader.center.text = "OAG Schedule Competitive Summary USA " + '\n&"Calibri"&8Sunday ' +  formatted_date
+ws.oddHeader.center.text = "OAG Schedule Competitive Summary" + region + '\n&"Calibri"&8Sunday ' +  formatted_date
 ws.oddHeader.center.size = 14
 ws.oddHeader.center.font = "Calibri,Bold"
 
@@ -260,7 +199,6 @@ num_columna18 = 15
 num_columna19 = 14
 
 # Eliminar la columnas
-
 ws.delete_cols(num_columna)
 ws.delete_cols(num_columna2)
 ws.delete_cols(num_columna17)
@@ -270,12 +208,8 @@ ws.delete_cols(num_columna19)
 # Guardar el archivo con modificación de scale al 110%
 ws.page_setup.scale = 110
 
-#Cambiar la region en el nombre del archivo dependiendo del archivo de origen
-region=''
-if re.match("^ReporteOAGLATAM",originFile):
-    region = " LATAM"
-elif re.match("^ReporteOAGUS",originFile):
-    region = " USA"
+#Repetir las cabeceras en excel sin agregarlas a los datos
+ws.print_title_rows = '1:1'
 
 # Guardar el archivo
 wb.save('OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + ' .xlsx')
@@ -284,46 +218,131 @@ wb.save('OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + 
 xlsx2html('OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + ' .xlsx', 'OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + ' .html')
 
 #Modificar el codigo html para ajustarlo al formato
+#Leer archivo html
 with open('OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + ' .html', "r") as f:
     text = f.read()
+
+#Definir el formato de la tabla, cabeceras y pie de pagina
 soup = BeautifulSoup(text, "html.parser")
 new_div = soup.new_tag("style")
 new_div.string = " table {font-family: Calibri; transform: scale(0.85,1); margin-left: auto; margin-right: auto;} @page{size: 8.3in 11.8in;} div.header {display: block; text-align: center; position: running(header); font-family: Calibri; transform: scale(1, 1);} div.footer { display: block; text-align: left; position: running(footer); font-family: Calibri; transform: scale(1, 1); } @page { @top-center { content: element(header) }} @page { @bottom-left { content: element(footer) }} @page {@bottom-right{content: counter(page)' - 'counter(pages); font-family: Calibry; font-size: 10.0px; margin-right: -55px;}}"
 soup.html.insert(1, new_div)
 
+#Insertar tag para cabecera
 new_divH = soup.new_tag("div")
 soup.html.body.insert(1, new_divH)
 soup.select('div')[0]['class'] = 'header'
 
+#Insertar tag en cabecera con el titulo del reporte
 new_divH1 = soup.new_tag("h1")
 new_divH1.string = "OAG Schedule Competitive Summary " + region
 soup.html.body.div.insert(1, new_divH1)
 soup.select('h1')[0]['style'] = 'font-size: 19.0px; font-weight: normal;'
 
+#Insertar tag en cabecera con fecha del reporte
 new_divH2 = soup.new_tag("p")
 new_divH2.string = "Sunday " + formatted_date
 soup.html.body.div.insert(2, new_divH2)
 soup.select('p')[0]['style'] = 'font-size: 13.0px'
 
-
+#Insertar tag de pie de pagina
 new_divF = soup.new_tag("div")
 new_divF.string ="Prev Snap:  " + formatted_date3 + " New Snap: " + formatted_date2
 soup.html.body.insert(2, new_divF)
 soup.select('div')[1]['class'] = 'footer'
 soup.select('div')[1]['style'] = 'font-size: 10.0px; margin-left: -55px;'
 
+#Agregar cabeceras de tabla en cada página
+style_left = "background-color: #BFBFBF;border-bottom: none;border-collapse: collapse;border-left: none;border-right: none;border-top: none;font-size: 9.0px;font-weight: bold;height: 17.25pt;text-align: left; word-spacing: 20px; line-height: 130%;; transform: scale(1.2,1);"
+style_center = "background-color: #BFBFBF;border-bottom: none;border-collapse: collapse;border-left: none;border-right: none;border-top: none;font-size: 9.0px;font-weight: bold;height: 17.25pt;text-align: center; word-spacing: 20px; line-height: 130%;; transform: scale(1.2,1);"
+
+total_rows = num_rows + int(num_rows/79)+2
+for max_row in range(80, total_rows, 79):
+        original_tag=soup.new_tag("tr")
+
+        new_td1 = soup.new_tag("td", id="Sheet!A0", style=style_left)
+        new_td1.string = "CA"
+        original_tag.append(new_td1)
+
+        new_td2 = soup.new_tag("td", id="Sheet!B0", style=style_left)
+        new_td2.string = "Market"
+        original_tag.append(new_td2)
+
+        new_td3 = soup.new_tag("td", id="Sheet!C0", style=style_left)
+        new_td3.string = "Ind AM"
+        original_tag.append(new_td3)
+
+        new_td4 = soup.new_tag("td", id="Sheet!D0", style=style_left)
+        new_td4.string = "Region"
+        original_tag.append(new_td4)
+
+        new_td5 = soup.new_tag("td", id="Sheet!E0", style=style_left)
+        new_td5.string = "Month"
+        original_tag.append(new_td5)
+
+        new_td6 = soup.new_tag("td", id="Sheet!F0", style=style_left)
+        new_td6.string = "Chg"
+        original_tag.append(new_td6)
+
+        new_td7 = soup.new_tag("td", id="Sheet!G0", style=style_center)
+        new_td7.string = "Prev Ops"
+        original_tag.append(new_td7)
+
+        new_td8 = soup.new_tag("td", id="Sheet!H0", style=style_center)
+        new_td8.string = "New Ops"
+        original_tag.append(new_td8)
+
+        new_td9 = soup.new_tag("td", id="Sheet!I0", style=style_center)
+        new_td9.string = "Ops Chg"
+        original_tag.append(new_td9)
+
+        new_td10 = soup.new_tag("td", id="Sheet!J0", style=style_center)
+        new_td10.string = "Prev Seat"
+        original_tag.append(new_td10)
+
+        new_td11 = soup.new_tag("td", id="Sheet!K0", style=style_center)
+        new_td11.string = "New Seat"
+        original_tag.append(new_td11)
+
+        new_td12 = soup.new_tag("td", id="Sheet!L0", style=style_center)
+        new_td12.string = "Seat Chg"
+        original_tag.append(new_td12)
+
+        new_td13 = soup.new_tag("td", id="Sheet!M0", style=style_center)
+        new_td13.string = "%_Seat Chg"
+        original_tag.append(new_td13)
+
+        soup.html.body.table.insert(max_row * 2, original_tag)
+
+last_tag = soup.find_all("tr")[-1]
+if last_tag.select("td")[0]["id"] == "Sheet!A0":
+    last_tag.extract()
+
+#Cambiar el formato de los datos
 for link in soup.findAll('td'):
+    #Ajustar el tamaño de letra de las celdas
     link['style'] = link['style'].replace('font-size: 6.0px','font-size: 9.0px') 
+
+    #Ajustar el tamaño de letra de las cabeceras 
     link['style'] = link['style'].replace('font-size: 11.0px','font-size: 5.0px')
+
+    #Ajustar el alto de los renglones en blanco (los que van despues de los totales)
     link['style'] = link['style'].replace("background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left-color: #BFBFBF;border-left-style: solid;border-left-width: 1px;border-right-color: #F2F2F2;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 7.5pt","background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left-color: #BFBFBF;border-left-style: solid;border-left-width: 1px;border-right-color: #F2F2F2;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 5.7pt;")    
     link['style'] = link['style'].replace("background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left: none;border-right-color: #F2F2F2;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 7.5pt","background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left: none;border-right-color: #F2F2F2;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 5.7pt")
     link['style'] = link['style'].replace("background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left: none;border-right-color: #BFBFBF;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 7.5pt","background-color: #FFFFFF;border-bottom: none;border-collapse: collapse;border-left: none;border-right-color: #BFBFBF;border-right-style: solid;border-right-width: 1px;border-top: none;font-size: 5.0px;height: 5.7pt")
+    
+    #Ajustar las cabeceras para que el texto aparezca en dos renglones y ajustar el interlineado
     if "bold;height: 17.25pt;text-align: " in link['style']:
         link['style'] += "; word-spacing: 20px; line-height: 130%;"
+    
+    #Ajustar la escala de la letra de las celdas
     link['style'] += "; transform: scale(1.2,1);"
+
+    #Agregar padding para que los datos en las celdas no se corten
     if not (re.match('^Sheet!M', link['id'])):
         link['style'] += " padding-right: 9px;"
 
+#Guardar los cambios en el archivo html
 with open('OAG Schedule Competitive Summary Sunday, ' + formatted_date + region + ' .html', "w") as f:
     f.write(soup.prettify(formatter="html"))
 
